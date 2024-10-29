219c34120d5c51c7591100734c72a88616f01850
Praise Aribisala <aribisalapraise12@gmail.com>
2024-10-29 15:27:40 +0100

    Task 0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+from basic_caching import BaseCaching
+"""Basic Dictionary"""
+
+
+class BasicCache(BaseCaching):
+    """Basic Dictionary"""
+
+    def __init__(self):
+        super.__init__()
+
+    def put(self, key, item):
+        """
+        Put item into cache with given key.
+        Args:
+            key (str): The key for the item
+            item (any): The item to cache
+        """
+        if key is None or item is None:
+            return None
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Get item from cache with given key.
+        Args:
+            key (str): The key for the item
+        Returns:
+        any: The item from cache if found, otherwise None
+        """
+        if key is None or key not in self.cache_data:
+            return None
+
+        value = self.cache_data.get(key)
+        return value
+
+
