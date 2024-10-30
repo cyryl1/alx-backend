f361e4cdc3642d705096bfdc42a68aafb89dee56
Praise Aribisala <aribisalapraise12@gmail.com>
2024-10-30 07:26:53 +0100

    Task 1

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+from basic_caching import BaseCaching
+from collections import OrderedDict
+"""FIFO Cache"""
+
+
+class FIFOCache(BaseCaching):
+    """A class representation for FIFO Caching"""
+
+    def __init__(self):
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """
+        Put item into cache with given key.
+        Args:
+            key (str): The key for the item
+            item (any): The item to cache
+        """
+        if key is None or item is None:
+            return None
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_key, _ = self.cache_data.popitem(last=False)
+            print(f"DISCARD: {first_key}")
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Get item from cache with given key.
+        Args:
+            key (str): The key for the item
+        Returns:
+        any: The item from cache if found, otherwise None
+        """
+        if key is None or key not in self.cache_data:
+            return None
+
+        return self.cache_data.get(key)
