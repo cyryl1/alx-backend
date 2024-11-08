9ce68c2f312460691308192f10a78b9336e5a3fd
Praise Aribisala <aribisalapraise12@gmail.com>
2024-11-08 16:33:58 +0100

    TAsk 1

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+"""Initialize flask application"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -5,6 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    """Render the template index.html for the root URL."""
     return render_template('0-index.html')
 
 
